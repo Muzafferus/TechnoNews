5a6b5ff7f4400f970135bf294e7c4fe5632fe1bc
muzafferus <muzefferpasazade@gmail.com>
2023-05-07 19:57:50 +0400

    Initial Commit

---
+++ app/src/main/java/com/muzafferus/technonews/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.muzafferus.technonews
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.muzafferus.technonews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
