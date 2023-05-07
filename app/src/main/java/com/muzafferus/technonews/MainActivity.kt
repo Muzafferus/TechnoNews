f8a891d6bad7d62d9c9941df14fe85d0974850d1
muzafferus <muzefferpasazade@gmail.com>
2023-05-07 20:20:10 +0400

    Add Dependencies

--- app/src/main/java/com/muzafferus/technonews/MainActivity.kt
+++ app/src/main/java/com/muzafferus/technonews/MainActivity.kt
@@ -3,7 +3,9 @@ package com.muzafferus.technonews
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.muzafferus.technonews.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
